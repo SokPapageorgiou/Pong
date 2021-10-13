801b2e194ca72e884becc00d0c60d250d2c3d6c5
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2021-10-13 10:58:19 +0200

    Ball Scriptable Objects Stats

---
+++ Assets/Scripts/Ball/BallStats.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/BallStats", fileName = "BallStats") ]
+public class BallStats : ScriptableObject
+{
+    public int speed;
+}
